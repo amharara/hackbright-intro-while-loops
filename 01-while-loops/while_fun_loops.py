5e4fe4d6353cf8e4d2a4c5ae81e5c164d30e5ba3
Christina Jensen <cj.christina.jensen@gmail.com>
2015-11-23 20:00:06 -0800

    first of the night

---
+++ 01-while-loops/while_fun_loops.py
@@ -0,0 +1,42 @@
+# #i=0
+# #while (i<=20):
+# 	print i,
+# 	i=i+1
+
+# i=0
+# while (i<=20):
+# 	if (i == 13):
+# 		print "hello"
+# 		i=i+1
+# 	elif (i !=13):
+# 		print i,
+# 		i=i+1
+
+
+# i=0
+# while (i<=10):
+# 	print i*10,
+# 	i=i+1	
+
+# i=0
+# while (i<=10):
+# 	if (i%2 != 0):
+# 		print i,
+# 	i= i + 1
+
+# i=10
+# while (i<=10):
+# 	if i<0:
+# 		break
+# 	print i,
+# 	i= i - 1
+
+i=10
+while (i<=10):
+	if i==0:
+		print "Blastoff"
+	elif i<0:
+		break
+	elif i!=0:
+		print i,
+	i= i - 1
